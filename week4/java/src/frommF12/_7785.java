5766a315ccb887ec89559851dd67a58441a7e7a6
hansanhha <83945336+frommF12@users.noreply.github.com>
2022-12-20 12:34:37 +0900

    Update _7785.java

--- week4/java/src/frommF12/_7785.java
+++ week4/java/src/frommF12/_7785.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
+// 20:17
 public class _7785 {
 
     public static void main(String[] args) throws IOException {
