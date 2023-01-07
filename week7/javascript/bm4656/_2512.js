ba5fe98bc2631da68eb8b96734f96ed477694c10
bm4656 <newjin46@gmail.com>
2023-01-07 23:54:03 +0900

    _2512.js

---
+++ week7/javascript/bm4656/_2512.js
@@ -0,0 +1,12 @@
+//
+function solution(data) {}
+
+const case1 = `4
+  120 110 140 150
+  485`;
+
+const case2 = `4
+  120 110 140 150
+  485`;
+console.log(solution(case1));
+console.log(solution(case2));
