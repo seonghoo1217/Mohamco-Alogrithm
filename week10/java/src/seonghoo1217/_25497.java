463266d181cf2f69eadcefe0afd4bdaaa73a364d
seonghoo1217 <seonghoo1217@naver.com>
2023-02-04 00:16:32 +0900

    _25497스택 수정

--- week10/java/src/seonghoo1217/_25497.java
+++ week10/java/src/seonghoo1217/_25497.java
@@ -1,23 +1,34 @@
 package seonghoo1217;
 
 import java.io.*;
+import java.util.Stack;
 
 public class _25497 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int S=0,L=0;
 		int answer=0;
-		br.readLine();
+		int N = Integer.parseInt(br.readLine());
 		String skill = br.readLine();
 		boolean check=true;
-		for (int i=0;check&&i<skill.length();i++){
+		Stack<Character> stack=new Stack<>();
+		for (int i=0;i<N;i++){
+			char part = skill.charAt(i);
+			if (part=='S') S++;
+			if (part=='L') L++;
+			stack.push(part);
+		}
+
+		for (int i=0;check&&i<N;i++){
 			check=true;
-			switch (skill.charAt(i)){
-				case 'L': L++; break;
-				case 'S': S++; break;
-				case 'R': if (L==0) check=false;else {answer++;L--;} break;
-				case 'K': if (S==0) check=false;else {answer++;S--;} break;
-				default:answer++;
+			char peek = stack.peek();
+			switch (peek){
+				case 'S':
+				case 'L':
+					stack.pop(); break;
+				case 'R': if (L==0) check=false;else {answer++;L--;stack.pop();} break;
+				case 'K': if (S==0) check=false;else {answer++;S--;stack.pop();} break;
+				default:answer++; stack.pop();
 			}
 		}
 		System.out.println(answer);
