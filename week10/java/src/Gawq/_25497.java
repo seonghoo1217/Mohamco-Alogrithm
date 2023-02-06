9d248203e172c1b578f4a8740765632b8d6e0bb7
Gawq <cnj991279@gmail.com>
2023-02-06 15:48:21 +0900

    rename

--- week10/java/src/Gawq/_25497.java
+++ week10/java/src/Gawq/_25497.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _25479 {
+public class _25497 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
